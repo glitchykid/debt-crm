61cbdc1b3f279c037c5e95a74c6f192e8c25beed
glitchykid <glitchykid.next@icloud.com>
2026-03-18 12:01:13 +0300

    build: add mui

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Jost } from "next/font/google";
+import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/theme";
+
+const jost = Jost({
+  variable: "--font-jost-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Debt CRM",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${jost.variable} antialiased`}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+
+export default function Home() {
+  return (
+    <Container>
+      <Button variant="contained">Hello world!</Button>
+    </Container>
+  );
+}
